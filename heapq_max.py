34a233d325c0072c655e7a7e5b2c36b350f2d7c3
Zhe He <hezhe88@gmail.com>
2016-07-26 22:32:56 -0400

    First commit

---
+++ heapq_max.py
@@ -0,0 +1,115 @@
+"""
+Modified (very slightly) from heapq.py.
+https://github.com/python/cpython/blob/master/Lib/heapq.py
+
+Differece: instead of the min-heap in heapq module, this is a max-heap.
+
+Usage:
+
+tl;dr: same as heapq module except adding '_max' to all functions.
+
+heap_max = []            # creates an empty heap
+heappush_max(heap_max, item) # pushes a new item on the heap
+item = heappop_max(heap_max) # pops the largest item from the heap
+item = heap_max[0]       # largest item on the heap without popping it
+heapify_max(x)        # transforms list into a heap, in-place, in linear time
+item = heapreplace_max(heap_max, item) # pops and returns largest item, and
+                               # adds new item; the heap size is unchanged
+"""
+
+
+__all__ = ['heappush_max', 'heapreplace_max', 'heappushpop_max',
+           'heappop_max', 'heapreplace_max', 'heapify_max']
+
+
+def heappush_max(heap, item):
+    """Push item onto heap, maintaining the heap invariant."""
+    heap.append(item)
+    _siftdown_max(heap, 0, len(heap) - 1)
+
+
+def heappushpop_max(heap, item):
+    """Fast version of a heappush followed by a heappop."""
+    if heap and heap[0] > item:
+        # if item >= heap[0], it will be popped immediately after pushed
+        item, heap[0] = heap[0], item
+        _siftup_max(heap, 0)
+    return item
+
+
+def heappop_max(heap):
+    """Maxheap version of a heappop."""
+    lastelt = heap.pop()    # raises appropriate IndexError if heap is empty
+    if heap:
+        returnitem = heap[0]
+        heap[0] = lastelt
+        _siftup_max(heap, 0)
+        return returnitem
+    return lastelt
+
+
+def heapreplace_max(heap, item):
+    """Maxheap version of a heappop followed by a heappush."""
+    returnitem = heap[0]    # raises appropriate IndexError if heap is empty
+    heap[0] = item
+    _siftup_max(heap, 0)
+    return returnitem
+
+
+def heapify_max(x):
+    """Transform list into a maxheap, in-place, in O(len(x)) time."""
+    n = len(x)
+    for i in reversed(range(n // 2)):
+        _siftup_max(x, i)
+
+
+def _siftdown_max(heap, startpos, pos):
+    'Maxheap variant of _siftdown'
+    newitem = heap[pos]
+    # Follow the path to the root, moving parents down until finding a place
+    # newitem fits.
+    while pos > startpos:
+        parentpos = (pos - 1) >> 1
+        parent = heap[parentpos]
+        if parent < newitem:
+            heap[pos] = parent
+            pos = parentpos
+            continue
+        break
+    heap[pos] = newitem
+
+
+def _siftup_max(heap, pos):
+    'Maxheap variant of _siftup'
+    endpos = len(heap)
+    startpos = pos
+    newitem = heap[pos]
+    # Bubble up the larger child until hitting a leaf.
+    childpos = 2 * pos + 1    # leftmost child position
+    while childpos < endpos:
+        # Set childpos to index of larger child.
+        rightpos = childpos + 1
+        if rightpos < endpos and not heap[rightpos] < heap[childpos]:
+            childpos = rightpos
+        # Move the larger child up.
+        heap[pos] = heap[childpos]
+        pos = childpos
+        childpos = 2 * pos + 1
+    # The leaf at pos is empty now.  Put newitem there, and bubble it up
+    # to its final resting place (by sifting its parents down).
+    heap[pos] = newitem
+    _siftdown_max(heap, startpos, pos)
+
+# If available, use C implementation
+try:
+    from _heapq import _heapreplace_max as heapreplace_max
+except ImportError:
+    pass
+try:
+    from _heapq import _heapify_max as heapify_max
+except ImportError:
+    pass
+try:
+    from _heapq import _heappop_max as heappop_max
+except ImportError:
+    pass
