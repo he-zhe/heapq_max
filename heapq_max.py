f9587e2cbb5cfda35165eb432084cd044540cbf2
Zhe He <hezhe88@gmail.com>
2016-07-27 20:51:01 -0400

    import existed max functions directely from heapq, instead of code it again; add test cases

--- heapq_max.py
+++ heapq_max.py
@@ -18,6 +18,11 @@ item = heapreplace_max(heap_max, item) # pops and returns largest item, and
 """
 
 
+from heapq import _heapify_max as heapify_max
+from heapq import _heappop_max as heappop_max
+from heapq import _heapreplace_max as heapreplace_max
+from heapq import _siftdown_max, _siftup_max
+
 __all__ = ['heappush_max', 'heapreplace_max', 'heappushpop_max',
            'heappop_max', 'heapreplace_max', 'heapify_max']
 
@@ -35,81 +40,3 @@ def heappushpop_max(heap, item):
         item, heap[0] = heap[0], item
         _siftup_max(heap, 0)
     return item
-
-
-def heappop_max(heap):
-    """Maxheap version of a heappop."""
-    lastelt = heap.pop()    # raises appropriate IndexError if heap is empty
-    if heap:
-        returnitem = heap[0]
-        heap[0] = lastelt
-        _siftup_max(heap, 0)
-        return returnitem
-    return lastelt
-
-
-def heapreplace_max(heap, item):
-    """Maxheap version of a heappop followed by a heappush."""
-    returnitem = heap[0]    # raises appropriate IndexError if heap is empty
-    heap[0] = item
-    _siftup_max(heap, 0)
-    return returnitem
-
-
-def heapify_max(x):
-    """Transform list into a maxheap, in-place, in O(len(x)) time."""
-    n = len(x)
-    for i in reversed(range(n // 2)):
-        _siftup_max(x, i)
-
-
-def _siftdown_max(heap, startpos, pos):
-    'Maxheap variant of _siftdown'
-    newitem = heap[pos]
-    # Follow the path to the root, moving parents down until finding a place
-    # newitem fits.
-    while pos > startpos:
-        parentpos = (pos - 1) >> 1
-        parent = heap[parentpos]
-        if parent < newitem:
-            heap[pos] = parent
-            pos = parentpos
-            continue
-        break
-    heap[pos] = newitem
-
-
-def _siftup_max(heap, pos):
-    'Maxheap variant of _siftup'
-    endpos = len(heap)
-    startpos = pos
-    newitem = heap[pos]
-    # Bubble up the larger child until hitting a leaf.
-    childpos = 2 * pos + 1    # leftmost child position
-    while childpos < endpos:
-        # Set childpos to index of larger child.
-        rightpos = childpos + 1
-        if rightpos < endpos and not heap[rightpos] < heap[childpos]:
-            childpos = rightpos
-        # Move the larger child up.
-        heap[pos] = heap[childpos]
-        pos = childpos
-        childpos = 2 * pos + 1
-    # The leaf at pos is empty now.  Put newitem there, and bubble it up
-    # to its final resting place (by sifting its parents down).
-    heap[pos] = newitem
-    _siftdown_max(heap, startpos, pos)
-
-# If available, use C implementation
-try:
-    from _heapq import _heapreplace_max as heapreplace_max
-except ImportError:
-    pass
-try:
-    from _heapq import _heapify_max as heapify_max
-except ImportError:
-    pass
-try:
-    from _heapq import _heappop_max as heappop_max
-except ImportError:
-    pass
---
+++ test.py
@@ -0,0 +1,65 @@
+import random
+import unittest
+from unittest import TestCase
+
+from heapq_max import heappop_max, heappush_max, heappushpop_max
+
+
+class TestHeap_max(TestCase):
+
+    def test_heappushpop(self):
+        h = []
+        x = heappushpop_max(h, 10)
+        self.assertEqual((h, x), ([], 10))
+
+        h = [10]
+        x = heappushpop_max(h, 10.0)
+        self.assertEqual((h, x), ([10], 10.0))
+        self.assertEqual(type(h[0]), int)
+        self.assertEqual(type(x), float)
+
+        h = [10]
+        x = heappushpop_max(h, 9)
+        self.assertEqual((h, x), ([9], 10))
+
+        h = [10]
+        x = heappushpop_max(h, 11)
+        self.assertEqual((h, x), ([10], 11))
+
+    def test_push_pop(self):
+        # 1) Push 256 random numbers and pop them off, verifying all's OK.
+        heap = []
+        data = []
+        self.check_invariant(heap)
+        for i in range(256):
+            item = random.random()
+            data.append(item)
+            heappush_max(heap, item)
+            self.check_invariant(heap)
+        results = []
+        while heap:
+            item = heappop_max(heap)
+            self.check_invariant(heap)
+            results.append(item)
+        data_sorted = data[:]
+        data_sorted.sort(reverse=True)
+        self.assertEqual(data_sorted, results)
+        # 2) Check that the invariant holds for a sorted array
+        self.check_invariant(results)
+
+        self.assertRaises(TypeError, heappush_max, [])
+        try:
+            self.assertRaises(TypeError, heappush_max, None, None)
+            self.assertRaises(TypeError, heappop_max, None)
+        except AttributeError:
+            pass
+
+    def check_invariant(self, heap):
+            # Check the heap invariant.
+        for pos, item in enumerate(heap):
+            if pos:  # pos 0 has no parent
+                parentpos = (pos - 1) >> 1
+                self.assertTrue(heap[parentpos] >= item)
+
+if __name__ == "__main__":
+    unittest.main()
